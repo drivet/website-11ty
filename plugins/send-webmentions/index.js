ba1ecda1d34b336651191aac41a81f52c199116e
Desmond Rivet <desmond.rivet@gmail.com>
2022-03-01 15:02:07 -0500

    Rename send_webmentions to send-webmentions

--- plugins/send-webmentions/index.js
+++ plugins/send-webmentions/index.js
@@ -23,8 +23,8 @@ async function getWmResultsSHA() {
 
 async function commitWmResults(wmresults) {
   try {
-    const yaml_str = yaml.dump(wmresults);
-    const encoded = Base64.encode(yaml_str);
+    const yamlStr = yaml.dump(wmresults);
+    const encoded = Base64.encode(yamlStr);
     const sha = await getWmResultsSHA();
     const response = await octokit.repos.createOrUpdateFileContents({
       owner: "drivet",
@@ -49,34 +49,34 @@ async function commitWmResults(wmresults) {
 }
 
 async function loadUrls() {
-  const feed_data = fs.readFileSync('_site/posts/feed_all.xml', 'utf8');
+  const feedData = fs.readFileSync('_site/posts/feed_all.xml', 'utf8');
   const patom = util.promisify(atom);
-  const feed = await patom(feed_data);
+  const feed = await patom(feedData);
   return feed.map(a => a.link.replace('https://desmondrivet.com/', ''));
 }
 
 function loadWebmentionResults() {
-  const wmresults_data = fs.readFileSync('./src/_data/wmresults.yaml', 'utf8');
-  return yaml.load(wmresults_data);
+  const wmresultsData = fs.readFileSync('./src/_data/wmresults.yaml', 'utf8');
+  return yaml.load(wmresultsData);
 }
 
 async function onSuccess({ utils }) {
   const urls = await loadUrls();
   const wmresults = loadWebmentionResults();
-  const site_url = wmresults.site_url;
+  const siteUrl = wmresults.site_url;
   const sources = urls.filter(u => wmresults.results[u] === undefined || wmresults.results[u] === null);
   if (sources.length === 0) {
     console.log('No webmentions to process');
     return;
   }
   console.log(`processing these source URLs for possible webmentions: ${JSON.stringify(sources)}`);
-  const report = await sendAllWebmentions(sources.map(s => `${site_url}/${s}`));
-  console.log(`webmentions (possibly) sent, showing results for ${site_url}`);
+  const report = await sendAllWebmentions(sources.map(s => `${siteUrl}/${s}`));
+  console.log(`webmentions (possibly) sent, showing results for ${siteUrl}`);
   for (const source of sources) {
     console.log(`Source URL: ${source}`);
-    const result_for_source = report[`${site_url}/${source}`] || {};
-    console.log(`Result: ${JSON.stringify(result_for_source)}`);
-    wmresults.results[source] = result_for_source;
+    const resultForSource = report[`${siteUrl}/${source}`] || {};
+    console.log(`Result: ${JSON.stringify(resultForSource)}`);
+    wmresults.results[source] = resultForSource;
   }
   await commitWmResults(wmresults);
 }
