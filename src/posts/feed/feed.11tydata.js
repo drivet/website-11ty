e9dbad166ed3ac95a5b120878a02623ec19181c4
Desmond Rivet <desmond.rivet@gmail.com>
2023-04-27 11:23:38 -0400

    Handle mastodon likes and reposts

--- src/posts/feed/feed.11tydata.js
+++ src/posts/feed/feed.11tydata.js
@@ -1,13 +1,38 @@
 const pt = require('../../../utils/post-type.js');
 const _ = require('lodash');
 
-function bridgyMentions(data) {
-  const properties = data;
+function containsOnlyNumbers(str) {
+  return str && /^\d+$/.test(str);
+}
+
+/**
+ * NOT FOOLPROOF, but should weed out the obviously wrong ones
+ * 
+ * @param url 
+ * @returns 
+ */
+function looksLikeMastodon(url) {
+  if (!url) {
+    return false;
+  }
+  const urlObj = new URL(url);
+  const pathParts = urlObj.pathname.split('/');
+  return pathParts[1] && pathParts[1].startsWith('@') && containsOnlyNumbers(pathParts[2]);
+}
+
+function bridgyMentions(properties) {
   if (!properties) {
     return [];
   }
 
   const syndicationTokens = properties['mp-syndicate-to'];
+  if (!syndicationTokens && looksLikeMastodon(properties['like-of'] || properties['repost-of'])) {
+    // the program I use to like and reposts, quill, doesn't let you stamp a 
+    // syndication property when you like or repost, so do it here if this is a like or repost 
+    // and it kind of looks like a mastodon link
+    return ['https://brid.gy/publish/mastodon?bridgy_omit_link=maybe'];
+  }
+
   if (!syndicationTokens) {
     return [];
   }
