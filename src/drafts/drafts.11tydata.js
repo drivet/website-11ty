21629877a21b707bf07ac7ddc90ec29f08c2cec9
Desmond Rivet <desmond.rivet@gmail.com>
2023-09-23 17:22:19 -0400

    Exckude drafts from collections in production

--- src/drafts/drafts.11tydata.js
+++ src/drafts/drafts.11tydata.js
@@ -1,10 +1,18 @@
+const isDevEnv = process.env.ELEVENTY_ENV === 'development';
 module.exports = {
   eleventyComputed: {
     permalink: data => {
-      if (process.env['ELEVENTY_ENV'] !== 'development') {
-        return false;
-      } else {
+      if (isDevEnv) {
         return `${data.page.filePathStem}.html`;
+      } else {
+        return false;
+      }
+    },
+    eleventyExcludeFromCollections: data => {
+      if (isDevEnv) {
+        return data.eleventyExcludeFromCollections;
+      } else {
+        return true;
       }
     }
   }
