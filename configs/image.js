550bbfaa19a2d29874fb8ba541eadf7c957f39fe
Desmond Rivet <desmond.rivet@gmail.com>
2025-11-22 08:28:10 -0500

    Leverage preview package (#619)

--- configs/image.js
+++ configs/image.js
@@ -1,12 +1,15 @@
 const Image = require("@11ty/eleventy-img");
+const Fetch = require("@11ty/eleventy-fetch");
+const debug = require('debug')('image');
+const icoToPng = require("ico-to-png");
 
-async function makeImage(src, widths) {
+async function makeImage(src, width, format) {
   try {
     return await Image(src, {
-      widths,
-      formats: ["jpeg"],
+      widths: [width],
+      formats: [format],
       urlPath: "/static/img/",
-      outputDir: "./_site/static/img/" ,
+      outputDir: "./_site/static/img/",
       cacheOptions: {
         duration: "*",
         directory: "_cache/images",
@@ -14,28 +17,35 @@ async function makeImage(src, widths) {
       },
     });
   } catch (e) {
-    console.warn(`failed to create image ${src}`);
+    debug(`cannot make image ${src}, ${width}, ${format}`);
     return null;
   }
 }
 
 async function atomImageShortcode(src, alt) {
-  const data = await makeImage(src, [800]); // match the width of regular image
+  // match the width of regular image
+  const data = await makeImage(src, 800, "jpeg");
+  if (!data) {
+    return `<img src="/static/img/avatar.png" alt=""/>`
+  }
   return `<img src="${data.jpeg[0].url}" alt="${alt}" width="600"/>`;
 }
 
 async function atomThumbShortcode(src, alt) {
-  const data = await makeImage(src, [200]); // match the width of thumb image
+  // match the width of thumb image
+  const data = await makeImage(src, 200, "jpeg");
+  if (!data) {
+    return `<img src="/static/img/avatar.png" alt=""/>`
+  }
   return `<img src="${data.jpeg[0].url}" alt="${alt}" width="200"/>`;
 }
 
-async function imgShortcode(src, alt, cls, widths, onerror) {
-  const data = await makeImage(src, widths);
+async function imgShortcode(src, alt, cls, width, onerror) {
+  const data = await makeImage(src, width, "jpeg");
   if (!data) {
     return `<img src="/static/img/avatar.png" alt=""/>`
   }
   const allCls = data['jpeg'][0].height > data['jpeg'][0].width ? `${cls} portrait` : cls;
-  //const sizes = '(min-width: 1024px) 444, (min-width: 768px) 444, 100vw';
   const attributes = {
     alt, class: allCls,
     loading: "lazy",
@@ -48,19 +58,19 @@ async function imgShortcode(src, alt, cls, widths, onerror) {
 }
 
 async function largeImageShortcode(src, alt, cls, onerror) {
-  return await imgShortcode(src, alt, cls, [1200], onerror);
+  return await imgShortcode(src, alt, cls, 1200, onerror);
 }
 
 async function imageShortcode(src, alt, cls, onerror) {
-  return await imgShortcode(src, alt, cls, [800], onerror);
+  return await imgShortcode(src, alt, cls, 800, onerror);
 }
 
 async function bigThumbShortcode(src, alt, cls, onerror) {
-  return await imgShortcode(src, alt, cls, [400], onerror);
+  return await imgShortcode(src, alt, cls, 400, onerror);
 }
 
 async function thumbShortcode(src, alt, cls, onerror) {
-  return await imgShortcode(src, alt, cls, [200], onerror);
+  return await imgShortcode(src, alt, cls, 200, onerror);
 }
 
 async function avatarShortcode(src, name) {
@@ -68,23 +78,44 @@ async function avatarShortcode(src, name) {
   const title = name;
   const width = 32;
   const staticAvatar = '/static/img/avatar.png';
-  if (!src) {
+  if (!src || !src.startsWith("http")) {
     return `<img src="${staticAvatar}" alt="${alt}" title="${title}" />`
   }
-  const data = await makeImage(src, [width]);
+
+  const data = await makePng(src, width);
   if (!data) {
-    return `<img src="${staticAvatar}" alt="${alt}" title="${title}" />`
+    return `<img src="${src}" alt="${alt}" title="${title}" />`
   }
+
   const cls = "u-photo avatar";
-  const allCls = data['jpeg'][0].height > data['jpeg'][0].width ? `${cls} portrait` : cls;
   const attributes = {
     alt,
     title,
-    class: allCls,
+    class: cls,
     onerror: `if (this.src != '${staticAvatar}') this.src = '${staticAvatar}';`,
   };
   return Image.generateHTML(data, attributes);
- 
+}
+
+async function makePng(src, width) {
+  if (src.split("?")[0].endsWith(".ico")) {
+    try {
+      const pngBuffer = await convertIcoToPng(src, width);
+      return await makeImage(pngBuffer, width, "png");
+    } catch (e) {
+      return await makeImage(src, width, "png");
+    }
+  } else {
+    return await makeImage(src, width, "png");
+  }
+}
+
+async function convertIcoToPng(href, width) {
+  const icoBuffer = await Fetch(href, {
+    type: "buffer",
+    dryRun: true,
+  });
+  return icoToPng(icoBuffer, width);
 }
 
 function imageConfig(eleventyConfig) {
